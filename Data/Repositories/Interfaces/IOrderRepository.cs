b3219af8f3d92664d515f158d1027ee8cab017d8
JakeStopford <jake_stopford@hotmail.com>
2023-12-14 22:43:10 +0000

    added repository

---
+++ Data/Repositories/Interfaces/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using Data.Entities;
+
+namespace Data.Repositories.Interfaces;
+
+public interface IOrderRepository
+{
+    Task<CustomerEntity?> GetCustomerByEmailAsync(string email);
+    Task<ShippingAddressEntity?> GetShippingAddressAsync(ShippingAddressEntity address);
+    Task<int> AddCustomerAsync(CustomerEntity customer);
+    Task<int> AddShippingAddressAsync(ShippingAddressEntity? shippingAddress);
+    Task<int> AddOrderAsync(OrderEntity order);
+    Task<int> SaveChangesAsync();
+}
---
+++ Data/Repositories/OrderRepository.cs
@@ -0,0 +1,55 @@
+﻿using Data.Context;
+using Data.Entities;
+using Data.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly DataContext _context;
+
+        public OrderRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<CustomerEntity?> GetCustomerByEmailAsync(string email)
+        {
+            return await _context.Customers.FirstOrDefaultAsync(x => x.Email == email);
+        }
+
+        public async Task<ShippingAddressEntity?> GetShippingAddressAsync(ShippingAddressEntity address)
+        {
+            return await _context.ShippingAddresses.FirstOrDefaultAsync(x =>
+                x.AddressLine1 == address.AddressLine1 &&
+                x.AddressLine2 == address.AddressLine2 &&
+                x.City == address.City &&
+                x.Country == address.Country &&
+                x.PostCode == address.PostCode);
+        }
+
+        public async Task<int> AddCustomerAsync(CustomerEntity customer)
+        {
+            _context.Customers.Add(customer);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> AddShippingAddressAsync(ShippingAddressEntity shippingAddress)
+        {
+            _context.ShippingAddresses.Add(shippingAddress);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> AddOrderAsync(OrderEntity order)
+        {
+            _context.Orders.Add(order);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
